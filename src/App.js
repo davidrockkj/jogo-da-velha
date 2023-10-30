2469477de162bd3260ca72ffe77d4ffe76a1295e
davidrockkj <davidrockkj@gmail.com>
2023-10-30 19:45:32 -0300

    Começando a arquitetura de pastas, usando a ITCSS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <a href='#' className='action'>
+      <a href='#to-do' className='action'>
         sobre
       </a>
       
