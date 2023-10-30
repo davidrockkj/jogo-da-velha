926df33a84600b3bdeae2ac213fc20a1b757c613
davidrockkj <davidrockkj@gmail.com>
2023-10-30 16:59:20 -0300

    Nav

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+import './Nav.css'
+
+function Nav() {
+  return (
+    <nav>
+      Logo
+      <div className="Sobre">
+        <p><strong>SOBRE</strong></p>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav;
