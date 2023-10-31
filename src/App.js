f4e27f9e27dc665e1b955ad10699c6c86783be64
davidrockkj <davidrockkj@gmail.com>
2023-10-31 16:49:39 -0300

    Criado o component Header sem CSS

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react'
 
-import Logo from './objects/Logo';
-import Sobre from './objects/Sobre'
-import Menu from './objects/Menu';
+import Header from './components/Header/index'
 
 function App() {
   return (
-    <> {/* Fragment*/}
-      <Logo />
-      <Sobre />
-      <Menu />
-      
-    </>
+    <Header />
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,23 @@
+/* COMPONENTE HEADER
+ * Logo
+ * Sobre
+ * Menu
+ */
+
+import React from 'react'
+
+import Logo from '../../objects/Logo'
+import Sobre from '../../objects/Sobre'
+import Menu from '../../objects/Menu'
+
+const Header = () => {  
+  return (
+    <header className='header'>
+        <Logo />
+        <Sobre />
+        <Menu />
+        
+    </header>
+  );
+}
+export default Header;
