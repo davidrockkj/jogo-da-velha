2c4a3069ead818e118330297f8a42eb123e6a912
davidrockkj <davidrockkj@gmail.com>
2023-11-06 17:46:51 -0300

    Aceitar dois jogadores no player

--- src/objects/Player/index.js
+++ src/objects/Player/index.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import './styles.css'
 import playerX from '../../img/player-x.png'
+import playerO from '../../img/player-o.png'
 
 /**
  * Será o objeto que, ao clicar, vai aparecer
  * um X ou um O
  */
 
-const Player = () => (
-  <button className='player'>
-    <img src={playerX} alt='Jogador X' />
-  </button>
-);
+const Player = ({player}) => {
+  const players = [];
+  players['x'] = playerX;
+  players['o'] = playerO;
+
+  return (
+    <button className='player'>
+    <img src={players[player]} alt={`Jogador ${player.toUpperCase()}`} />
+    </button>
+  );
+}
 
 export default Player;
