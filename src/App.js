48f8fc982c9af9d046fe28187fd92b28f85f793f
davidrockkj <davidrockkj@gmail.com>
2023-11-06 17:01:16 -0300

    Criado o object Card, apenas o JSX esta faltando o CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
+import Card from './objects/Card';
+
 import Header from './components/Header/index'
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Card />
+    </>
   );
 }
 
