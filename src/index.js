f7ea54cd3a777828a23d6a98a926a6f33e329442
davidrockkj <davidrockkj@gmail.com>
2023-11-06 18:54:07 -0300

    Deifinindo o layout básico do component Hashtag

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './styles/settings/colors.css'
 import './styles/settings/spacing.css'
 import 'normalize.css';
+import './styles/generic/reset.css'
 import './styles/elements/base.css'
 import App from './App';
 
