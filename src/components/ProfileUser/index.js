48272f7922ebaad081e9dcde0fd4121a1288609e
davidrockkj <davidrockkj@gmail.com>
2024-01-18 15:13:30 -0300

    Criado o css de ProfileUser

--- src/components/ProfileUser/index.js
+++ src/components/ProfileUser/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import './styles.css';
 import Profile from '../../img/davidrocha.png'
+
 import AvatarProfile from '../../objects/AvatarProfile';
 
 const ProfileUser = () => (
-  <dl>
-    <dd>
+  <dl className='profile-user'>
+    <dd className='avatar'>
       <AvatarProfile src={Profile} alt='Imagem David Rocha' />
     </dd>
-    <dt>David Rocha</dt>
-    <dd>Dev Front-end React e Mobile com React Native. </dd>
+    <dt className='title'>David Rocha</dt>
+    <dd className='description'>Dev Front-end React e Mobile com React Native. </dd>
   </dl>
 );
 
