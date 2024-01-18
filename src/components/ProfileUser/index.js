8c0cbbf0e3eb628b31bf3841fc484f827d29c503
davidrockkj <davidrockkj@gmail.com>
2024-01-18 16:04:52 -0300

    Criado o object IconSocial

--- src/components/ProfileUser/index.js
+++ src/components/ProfileUser/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './styles.css';
 import Profile from '../../img/davidrocha.png'
+import github from '../../img/github.png'
+import linkedin from '../../img/linkedin.png'
+import instagram from '../../img/instagram.png'
+import twitter from '../../img/twitter.png'
 
 import AvatarProfile from '../../objects/AvatarProfile';
+import IconSocial from '../../objects/IconSocial';
 
 const ProfileUser = () => (
   <dl className='profile-user'>
@@ -11,6 +16,12 @@ const ProfileUser = () => (
     </dd>
     <dt className='title'>David Rocha</dt>
     <dd className='description'>Dev Front-end React e Mobile com React Native. </dd>
+    <dd className='socials'>
+      <IconSocial src={github} alt="Logo da Github" />
+      <IconSocial src={linkedin} alt="Logo da Linkedin" />
+      <IconSocial src={instagram} alt="Logo da Instagram" />
+      <IconSocial src={twitter} alt="Logo da Twitter" />
+    </dd>
   </dl>
 );
 
